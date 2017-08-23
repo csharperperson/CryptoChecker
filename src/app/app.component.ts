6b0b8567eea6666771de6445fbaf7c0246413714
Tyler Moore <tylermoore124@gmail.com>
2017-08-23 19:57:54 -0400

    Setup api for checking crypto currency prices and status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { OnInit } from '@angular/core';
+
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'CryptoChecker';
+
+  constructor(private http: HttpClient) {}
+
+  currencies: any;
+  baseUrl = 'https://api.coinmarketcap.com/v1';
+
+  ngOnInit(): void {
+    // Make the HTTP request:
+    this.http.get(this.baseUrl + '/ticker/?limit=10').subscribe(data => {
+      // Read the result field from the JSON response.
+      this.currencies = data;
+    });
+  }
 }
